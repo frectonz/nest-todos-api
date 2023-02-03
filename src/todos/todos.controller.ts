cab027b5f34ee68f24ae771bc0414e3ec1fec780
frectonz <fraol0912@gmail.com>
2023-02-03 09:18:08 +0300

    create a todo module

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 
+import { TodosService } from './todos.service';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+
+@UseGuards(JwtAuthGuard)
 @Controller('todos')
-export class TodosController {}
+export class TodosController {
+  constructor(private todosService: TodosService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body('title') title: string, @Req() req) {
+    console.log(req.user);
+    return this.todosService.create(title, req.user.userId);
+  }
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  async findAll(@Req() req) {
+    return this.todosService.findAll(req.user.userId);
+  }
+}
--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -4,4 +4,21 @@ import { PrismaService } from 'src/prisma/prisma.service';
 @Injectable()
 export class TodosService {
   constructor(private prisma: PrismaService) {}
+
+  create(title: string, userId: number) {
+    return this.prisma.todo.create({
+      data: {
+        title: title,
+        userId: userId,
+      },
+    });
+  }
+
+  findAll(userId: number) {
+    return this.prisma.todo.findMany({
+      where: {
+        userId: userId,
+      },
+    });
+  }
 }
