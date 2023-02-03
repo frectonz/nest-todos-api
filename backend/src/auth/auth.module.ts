d881eb48f6c412a111f4aefe2d8c84a840515ea6
frectonz <fraol0912@gmail.com>
2023-02-03 16:05:05 +0300

    increase expiration date

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -18,7 +18,7 @@ import { LocalStrategy } from './strategies/local.strategy';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '1d' },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
