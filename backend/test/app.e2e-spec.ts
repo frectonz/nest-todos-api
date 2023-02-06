f2a730c9fbf67d1801e7e87c14f00e6a672c7846
frectonz <fraol0912@gmail.com>
2023-02-06 07:53:25 +0300

    register e2e test

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -15,10 +15,12 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('/auth/register (POST)', async () => {
+    const req = await request(app.getHttpServer()).post('/auth/register').send({
+      username: 'test',
+      password: 'test',
+    });
+
+    expect(req.status).toBe(201);
   });
 });
