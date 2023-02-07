b00daad0c8d15b7ebde1eb40de749f6da78877c5
frectonz <fraol0912@gmail.com>
2023-02-07 15:00:29 +0300

    remove providers

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,17 +1,14 @@
 import { Module, OnModuleInit } from '@nestjs/common';
 
+import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { TodosModule } from './todos/todos.module';
-import { UsersService } from './users/users.service';
-
-import { AuthModule } from './auth/auth.module';
-
 import { PrismaModule } from './prisma/prisma.module';
+
 import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [AuthModule, UsersModule, TodosModule, PrismaModule],
-  providers: [PrismaService, UsersService],
 })
 export class AppModule implements OnModuleInit {
   constructor(private prisma: PrismaService) {}
